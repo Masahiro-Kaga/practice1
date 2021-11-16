fa05ee9aa47d4a585c99dcd8190400dc23823dfd
加賀正浩 <masahiro41802891@gmail.com>
2021-11-16 10:19:28 -0800

    start section16

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import React, { useState, useCallback, useMemo } from 'react';
-
-import './App.css';
-import DemoList from './components/Demo/DemoList';
-import Button from './components/UI/Button/Button';
+import SimpleInput from './components/SimpleInput';
 
 function App() {
-  const [listTitle, setListTitle] = useState('My List');
-
-  const changeTitleHandler = useCallback(() => {
-    setListTitle('New Title');
-  }, []);
-
   return (
     <div className="app">
-      <DemoList title={listTitle} items={useMemo(()=> [5, 3, 1, 10, 9],[]) }/>
-      <Button onClick={changeTitleHandler}>Change List Title</Button>
+      <SimpleInput />
     </div>
   );
 }
