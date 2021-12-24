b5be78b3f91d8f43588a76aa310d6a35edbb849e
MasahiroKaga <masahiro41802891@gmail.com>
2021-12-24 12:25:32 -0800

    252,3 Second half done(First try)

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,18 +1,14 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-const initialState = {
-    cartIsVisible:false
-}
+import { createSlice } from '@reduxjs/toolkit';
 
 const uiSlice = createSlice({
-    name:"ui",
-    initialState,
-    reducers:{
-        toggle(state){
-            state.cartIsVisible = !state.cartIsVisible
-        }
+  name: 'ui',
+  initialState: { cartIsVisible: false },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
     }
-})
+  }
+});
 
 export const uiActions = uiSlice.actions;
 
