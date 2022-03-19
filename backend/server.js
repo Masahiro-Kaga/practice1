f451a90135614ed079f0cb69eff79d9f095367ab
Masahiro Kaga <masahiro41802891@gmail.com>
2022-03-19 11:19:02 -0700

    Interect with dammy data.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+// const notes = require("./data/mock_data")
+const notes = require("./data/MOCK_DATA.json");
 
 const app = express();
+
 app.get("/",(req,res) =>{
     res.send("API is running..");
+})
+
+app.get("/api/notes",(req,res)=>{
+    res.json(notes);
 })
 
 app.listen(4000,console.log("Server started on Port 4000."));
