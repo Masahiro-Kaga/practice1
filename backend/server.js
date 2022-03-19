d52c0be100d85c9eb5933157d2ab412709c42de6
Masahiro Kaga <masahiro41802891@gmail.com>
2022-03-19 09:39:51 -0700

    Tested to connect server/API with http GET request.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 
 const app = express();
+app.get("/",(req,res) =>{
+    res.send("API is running..");
+})
 
 app.listen(4000,console.log("Server started on Port 4000."));
