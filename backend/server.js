3bbd9fbffec814d632f9fe99634b4276a602e3cb
Masahiro Kaga <masahiro41802891@gmail.com>
2022-03-19 11:28:53 -0700

    Use params to find certain data.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-// const notes = require("./data/mock_data")
-const notes = require("./data/MOCK_DATA.json");
+const notes = require("./data/mock_data")
+// const notes = require("./data/MOCK_DATA.json");
 
 const app = express();
 
@@ -10,6 +10,11 @@ app.get("/",(req,res) =>{
 
 app.get("/api/notes",(req,res)=>{
     res.json(notes);
+})
+
+app.get("/api/notes/:id/:id2",(req,res)=>{
+    const note = notes.find((n) => n._id === req.params.id);
+    console.log(req.params);
 })
 
 app.listen(4000,console.log("Server started on Port 4000."));
