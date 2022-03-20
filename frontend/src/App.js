7af835cca9f4e9716ab246405ca33397a73b5045
Masahiro Kaga <masahiro41802891@gmail.com>
2022-03-20 07:30:39 -0700

    Add toggle on menu in order to mobile screen.

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { Fragment } from 'react';
+import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+
+const App = ()=> {
+  return (
+    <Fragment>
+      <Header>
+
+      </Header>
+      <Footer>
+        
+      </Footer>
+    </Fragment>
+  );
+}
+
+export default App;
